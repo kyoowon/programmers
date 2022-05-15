29e43fc25145ef8cbef334b94f413c4dc91ff679
kyoowon <kyoowon95@gmail.com>
2022-05-15 22:20:51 +0900

    ✅ add(nodes) : add nodes event listener & node icons

--- CatPhotoAlbum/src/components/App.js
+++ CatPhotoAlbum/src/components/App.js
@@ -1,32 +1,48 @@
-import Nodes from './Node.js'
+import Nodes from './Node.js';
+import BreadCrumb from './BreadCrumb.js';
 
 // $ - 예약어를 피하기 위해 선택자로 전달된 변수를 명시하기 위해서 사용
 export default function App($app) {
-
-    // 초기에 시작되었을 때 상태를 확인 - test 예시 값으로 확인.
-    const initialState = {
+    this.state = {
+        isRoot : false,
         nodes : [
             {
                   "id": "1",
                   "name": "노란고양이",
-                  "type": "DIRECTORY",
+                  "type": "FILE",
                   "filePath": null,
                   "parent": null
               },
               {
                   "id": "3",
                   "name": "까만고양이",
-                  "type": "DIRECTORY",
+                  "type": "FILE",
                   "filePath": null,
                   "parent": null
-              },
-          ]
+              }
+          ],
+        depth : []
     }
+
+    // const breadCrumb = new BreadCrumb({
+    //     $app,
+    //     initialState: this.state.depth
+    // });
+
 
     // 이를 node라고 칭하고 이를 관리하기 위해 Nodes compoenent를 둠
     const node = new Nodes({
         $app,
-        initialState
-    })
+        initialState: {
+            isRoot : this.state.isRoot,
+            nodes : this.state.nodes
+        },
+        onClick : (node) =>{
+            if (node.type === 'DIRECTORY') {
 
+            } else if (node.type === 'FILE') {
+
+            }
+        }
+    });
 }
--- CatPhotoAlbum/src/components/Node.js
+++ CatPhotoAlbum/src/components/Node.js
@@ -1,6 +1,6 @@
 
 // 초기에 상태가 전달되어 온다면
-export default function Nodes({ $app, initialState }){
+export default function Nodes({ $app, initialState, onClick }){
     this.state = initialState
     this.$target = document.createElement('ul')
     $app.appendChild(this.$target);
@@ -11,11 +11,46 @@ export default function Nodes({ $app, initialState }){
         this.render()
     }
 
-    // state 업데이트 할 때마다 $target에 해당 state의 node를 그림
+    this.onClick = onClick;
+
+    // state 업데이트 할 때마다 $target에 해당 state의 node를 render
     this.render = () => {
-        this.$target.innerHTML = this.state.nodes.map((node) =>
-            `<li>${node.name}</li>`
-        )
+        if (this.state.nodes) {
+            const nodeTemplate = this.state.nodes.map(node => {
+                const iconPath = node.type === 'FILE' ? './assets/file.png' : './assets/directory.png';
+
+                return `
+                <div class="Node" data-node-id="${node.id}">
+                    <img src="${iconPath}" />
+                    <div>${node.name}</div>
+                <div>
+                `
+            }).join('')
+
+            this.$target.innerHTML = !this.state.isRoot ? `<div class="Node"><img src="/assets/prev.png"></div>${nodeTemplate}` : nodeTemplate
+        }
+
+
+
+        this.$target.querySelectorAll("node").forEach($node => {
+            $node.addEventListener('click', (e) => {
+                const { nodeId } = e.target.dataset;
+                const selectNode = this.state.nodes.find(node => node.id === nodeId)
+
+                if (selectNode){
+                    this.onClick(selectNode)
+                }
+            })
+        })
+
+
     }
+
+
+
+
     this.render()
+
+
+
 }
